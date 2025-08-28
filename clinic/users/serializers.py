6421054b6dd22be504b738d42dd3a264ddaecc86
Etsukooo <125716131+Etsukooo@users.noreply.github.com>
2025-08-28 10:01:40 +0800

    Add clinic/users/serializers.py 1756346499238

---
+++ clinic/users/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 
+                 'last_name', 'role', 'phone_number', 'date_of_birth', 'address']
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match.")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        password = validated_data.pop('password')
+        user = CustomUser.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 
+                 'role', 'phone_number', 'date_of_birth', 'address', 'date_joined']
+        read_only_fields = ['id', 'username', 'role', 'date_joined']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Invalid credentials.')
+            if not user.is_active:
+                raise serializers.ValidationError('User account is disabled.')
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError('Must include username and password.')
+
+
+class DoctorListSerializer(serializers.ModelSerializer):
+    """Serializer for listing doctors (for appointment booking)"""
+    full_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'first_name', 'last_name', 'full_name', 'email']
+    
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip() or obj.username
